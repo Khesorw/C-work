5b23c8694bf5eb8666e40e62e27750f32b66fce4
Hasibullah Yosufi <habibkhesrow2@gmail.com>
2022-01-18 23:51:20 -0500

    all the programs are working as expected result

--- C-WorkSpace/Lab1/bin2dec.c
+++ C-WorkSpace/Lab1/bin2dec.c
@@ -7,48 +7,25 @@ int main(int argv, char* argc[])
 
 
 
+int binary = 0,temp; //binary reads from user temp is for printf actual user input
+
+    unsigned int decimal = 0; // decimal equivalent of base 2
+    int toPower = 0; //is use for binary calcualtion get each base to power of n
+    printf("Enter binary number: ");
+    scanf("%d",&binary);
+    temp = binary;
 
 
+    while(binary != 0){
+        int getBase = binary % 10; //get reminder of binary divide by 10 bcz base 10 get reminder 
+        decimal += (getBase*pow(2,toPower)); //get actual decimal value of 1s and 0s base to the power n
+        binary /= 10;
+        toPower++;
 
 
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// int binary = 0,temp;
-
-//     unsigned int decimal = 0;
-//     int toPower = 0;
-//     printf("Enter binary number: ");
-//     scanf("%d",&binary);
-//     temp = binary;
-
-
-//     while(binary != 0){
-//         int getBase = binary % 10;
-//         decimal += (getBase*pow(2,toPower));
-//         binary /= 10;
-//         toPower++;
-
-
-//     }
-
-//     printf("The decimal equivalen of %d is %d\n",temp,decimal);
+    printf("The decimal equivalen of %d is %d\n",temp,decimal);
 
 
 
