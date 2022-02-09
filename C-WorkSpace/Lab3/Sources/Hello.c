926fdeab7357191af8419a46847163f6b6feb79a
Hasibullah Yosufi <habibkhesrow2@gmail.com>
2022-02-09 14:31:55 -0500

    Header file created to lab3 function definations

---
+++ C-WorkSpace/Lab3/Sources/Hello.c
@@ -0,0 +1,13 @@
+#include "Hello.h"
+
+  int sum(int x, int v){
+
+      return (x+v);
+  }
+
+
+    double sub(double d,double dd){
+
+
+        return (d-dd);
+    }
---
+++ C-WorkSpace/Lab3/Sources/Main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define NUM_MAX 234
+
+int main(void){
+
+    
+
+
+    printf("Hello world\n");
+
+    return (0);
+}
